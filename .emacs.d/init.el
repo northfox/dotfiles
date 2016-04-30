64ef9f9c2a96fd4491bf57d13691bc5fa44a3c6c
northfox <vulpes.engineer@gmail.com>
2016-04-30 21:41:07 +0900

    Update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -39,6 +39,7 @@
     async
     auto-complete
     coffee-mode
+    company
     dash
     emmet-mode
     epl
@@ -61,6 +62,8 @@
     rainbow-mode
     rinari
     smartparens
+    tide
+    typescript-mode
     undo-tree
     undohist
     volatile-highlights
@@ -125,6 +128,35 @@
          (message "done")))
 ))
 
+;; typescript-mode
+(autoload 'typescript-mode "typescript-mode" "Major mode for editing by TypeScript." t)
+(add-to-list 'auto-mode-alist '("\\.ts$" . typescript-mode))
+(eval-after-load "typescript-mode"
+  '(progn
+     (require 'tide)
+     (add-hook 'typescript-mode-hook
+               (lambda ()
+                 (tide-setup)
+                 (flycheck-mode t)
+                 (setq flycheck-check-syntax-automatically '(save mode-enabled))
+                 (eldoc-mode t)
+                 (company-mode-on)))
+     (require 'company)
+     ;; C-n, C-pで補完候補を選べるように
+     (define-key company-active-map (kbd "M-n") nil)
+     (define-key company-active-map (kbd "M-p") nil)
+     (define-key company-active-map (kbd "C-n") 'company-select-next)
+     (define-key company-active-map (kbd "C-p") 'company-select-previous)
+     ;; C-hがデフォルトでドキュメント表示にmapされているので、文字を消せるようにmapを外す
+     (define-key company-active-map (kbd "C-h") nil)
+     ;; 1つしか候補がなかったらtabで補完、複数候補があればtabで次の候補へ行くように
+     (define-key company-active-map (kbd "<tab>") 'company-complete-common-or-cycle)
+     ;; ドキュメント表示
+     (define-key company-active-map (kbd "M-d") 'company-show-doc-buffer)
+
+     (setq company-minimum-prefix-length 1) ;; 1文字入力で補完されるように
+     ;; 候補の一番上でselect-previousしたら一番下に、一番下でselect-nextしたら一番上に行くように
+     (setq company-selection-wrap-around t)))
 
 ;; coffee-mode
 (autoload 'coffee-mode "coffee-mode" "Major mode for editing by CoffeeScript." t)
